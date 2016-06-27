e5e85ec575b17924a1f825e36179c89b4111dc80
Luis07 <luisbrenes250594@gmail.com>
2016-06-27 15:46:17 -0600

    Commit con helpers

---
+++ Prueba/WebApplication1/Helpers/OwnHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Helpers
+{
+    public static class OwnHelpers
+    {
+
+        public static MvcHtmlString Usuario(this HtmlHelper helper)
+        {
+            var result="<input type=text placeholder=Usuario>";
+            return new MvcHtmlString(result);
+        }
+        public static MvcHtmlString Password(this HtmlHelper helper)
+        {
+            var result = "<input type=password placeholder=Password>";
+            return new MvcHtmlString(result);
+        }
+
+        public static MvcHtmlString Apellido(this HtmlHelper helper)
+        {
+            var result = "<input type=text placeholder=Apellido>";
+            return new MvcHtmlString(result);
+        }
+        public static MvcHtmlString Ubicacion(this HtmlHelper helper)
+        {
+            var result = "<input type=text placeholder=Ubicacion>";
+            return new MvcHtmlString(result);
+        }
+    }
+}
